217a62282e20e18de6ed35575943b86548d8efaf
Hasala <hasaladissanayake@yahoo.com>
2023-07-21 16:46:27 +0530

    fixes in task 01

--- Task1/client.py
+++ Task1/client.py
@@ -1,27 +1,39 @@
 import socket
 import sys
 
+
 def start_client(server_ip, port):
+    
     client_socket = socket.socket()
     client_socket.connect((server_ip, port))
-    
+
     print(f"Connected to server {server_ip} on {port}")
-    
+
     while True:
         message = input("Enter a message (type 'terminate' to exit): ")
         client_socket.send(message.encode())
         if message == "terminate":
             break
-    
+
     print("Disconnected from server")
-    
+
     client_socket.close()
 
+
+# main function
 if __name__ == '__main__':
+
+    # Checking for the required format
     if len(sys.argv) != 3:
         print("FORMAT: python client.py SERVER_IP PORT")
         sys.exit(1)
 
     server_ip = sys.argv[1]
     port = int(sys.argv[2])
+
+    # Checking for the valid port number
+    if(port <= 1024 or port > 65535):
+        print("Invalid port number (Try a port between 1025 and 65535)")
+        sys.exit(1)
+    
     start_client(server_ip, port)
--- Task1/server.py
+++ Task1/server.py
@@ -1,31 +1,43 @@
 import socket
 import sys
 
+
 def start_server(port):
+
     server_socket = socket.socket()
+    # bind the socket to localhost ip address which is 127.0.0.1 and port number
     server_socket.bind(('localhost', port))
     server_socket.listen()
-    
+
     print(f"Server started and listening on port {port}")
-    
+
     client_socket, client_address = server_socket.accept()
     print(f"Client connected from {client_address}")
-    
+
     while True:
         data = client_socket.recv(1024).decode()
         if data == "terminate":
             break
         print(f"Received from client: {data}")
-    
-    print("Client disconnected")
-    
+
+    print(f"Client {client_address} disconnected")
+
+    # close the client socket
     client_socket.close()
-    server_socket.close()
+
 
 if __name__ == '__main__':
+
+    # Checking for the required format
     if len(sys.argv) != 2:
         print("FORMAT: python server.py PORT")
         sys.exit(1)
 
     port = int(sys.argv[1])
+
+    # Checking for the valid port number
+    if(port <= 1024 or port > 65535):
+        print("Invalid port number (Try a port between 1025 and 65535)")
+        sys.exit(1)
+
     start_server(port)
